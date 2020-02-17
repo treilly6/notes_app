0d7f0dfd3e09291dcaa20bf04487336c9092b4e5
treilly6 <treilly6@binghamton.edu>
2020-02-16 19:53:04 -0500

    config the pyinstaller

--- notes.py
+++ notes.py
@@ -4,6 +4,7 @@ from datetime import datetime
 class NotesApp:
     # initialize the instance
     def __init__(self):
+        print("In the __init__")
         # create the window
         self.gui = tk.Tk()
 
@@ -28,16 +29,21 @@ class NotesApp:
 
     # get the content from the text_box
     def get_text_value(self):
+        print("In the get text value")
         return self.text_box.get("1.0", "end-1c")
 
     # format the note
     def format_note_entry(self):
+        print("In format note entry")
         # get the typed text
         text_content = self.get_text_value()
+
         # get the timestamp
         timestamp = self.get_date()
+
         # format the note
         note = timestamp + '\n' + text_content + '\n'
+
         # append to notes file
         self.append_to_file(note)
 
@@ -46,15 +52,19 @@ class NotesApp:
 
     # method for clearing the text box
     def clear_text_box(self):
+        print("clearing the text box")
         self.text_box.delete("1.0", "end-1c")
 
     # get the timestamp for the note
     def get_date(self):
+        print("getting the date")
         now = datetime.now()
         return now.strftime("%m/%d/%Y %I:%M %p")
 
     # append the content to the file
     def append_to_file(self, note):
+        print("In append to file function")
+
         # open the note file
         notes_file = open("notes.txt", "a");
 
@@ -64,4 +74,5 @@ class NotesApp:
         # close the file
         notes_file.close()
 
+print("About to initiate the NotesApp")
 NotesApp()
